c098dc52a3d177c1a0062e08e973b1ed8485d0c6
Jastin123423 <Jjovinatha@gmail.com>
2026-01-13 19:56:04 +0300

    Delete functions directory

--- functions/[[path]].ts
+++
@@ -1,9 +0,0 @@
-
-// Catch-all route to handle missing endpoints gracefully with JSON
-export const onRequest = () => new Response(JSON.stringify({ error: "Endpoint not found" }), { 
-  status: 404, 
-  headers: {
-    "Content-Type": "application/json",
-    "Access-Control-Allow-Origin": "*"
-  } 
-});
