f1e0289fa0bbf1d609ec3a71aaf3edbfc2cce5cb
Jastin123423 <Jjovinatha@gmail.com>
2026-01-25 23:27:59 +0300

    Update group-post-likes.ts

--- functions/api/group-post-likes.ts
+++ functions/api/group-post-likes.ts
@@ -1,4 +1,4 @@
-// functions/api/group-post-likes.ts
+//. functions/api/group-post-likes.ts
 import type { PagesFunction } from "@cloudflare/workers-types";
 import { cors, ok, bad, server } from "./_cors";
 
