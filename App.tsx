35b710c404b363d563d4eec27e850dce84620bdc
Jastin123423 <Jjovinatha@gmail.com>
2026-02-07 23:43:10 +0300

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -1,3 +1,4 @@
+//App.tsx 
 import React, { useState, useEffect, useMemo, useCallback, useRef } from 'react';
 import { Login, Register } from './components/Auth';
 import { Header, Sidebar, RightSidebar } from './components/Layout';
