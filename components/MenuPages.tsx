e557822f9746f39f13e9cbf62a9408994f9c2fd1
Jastin123423 <Jjovinatha@gmail.com>
2026-02-08 18:53:49 +0300

    Update MenuPages.tsx

--- components/MenuPages.tsx
+++ components/MenuPages.tsx
@@ -4,7 +4,7 @@ import React, { useState, useEffect, useMemo } from 'react';
 import { User, Event, Group, Product, Post as PostType, AudioTrack } from '../types';
 import { MARKETPLACE_COUNTRIES } from '../constants';
 import { Post } from './Feed';
-export { default as EventsPage } from './EventsPage';
+
 
 
 
