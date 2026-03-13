39cdeaa84a84546173add02aadaaff31a8303951
Jastin123423 <Jjovinatha@gmail.com>
2026-03-13 16:30:27 +0300

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -23,7 +23,7 @@ import { StoryReel, CreateStoryModal, StoryViewerModal } from './components/Stor
 import { UserProfile } from './components/UserProfile';
 import { MarketplacePage, ProductDetailModal } from './components/Marketplace';
 import { ReelsFeed, CreateReelModal } from './components/Reels';
-import { AllEvents } from "./components/AllEvents";
+
 import { ImageViewer, ProfessionalLoader } from './components/Common';
 import {
   BirthdaysPage,
