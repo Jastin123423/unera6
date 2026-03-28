5d61390e307e40aab8c3cdb0def78ee996e96e89
Jastin123423 <Jjovinatha@gmail.com>
2026-03-28 16:25:15 +0300

    Update Feed.tsx

--- components/Feed.tsx
+++ components/Feed.tsx
@@ -5714,7 +5714,7 @@ export const CreatePost: React.FC<{
 
         <div
           className="flex items-center justify-center flex-1 gap-2 p-2 hover:bg-[#3A3B3C] rounded-lg cursor-pointer transition-colors"
-          onClick={onVideoClick}
+          onVideoClick={handleVideoClickFromCreate}
         >
           <i className="fas fa-camera text-[#F3425F] text-[24px]"></i>
           <span className="text-[#B0B3B8] font-bold text-[17px] hidden sm:block">
