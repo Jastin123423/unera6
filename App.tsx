89ccbe4a938073e1b23229a55765289d8b91bede
Jastin123423 <Jjovinatha@gmail.com>
2026-04-03 22:06:55 +0300

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -7820,6 +7820,27 @@ return (
       />
     )}
 
+    {showStoryFeeds && selectedStory && (
+  <StoryFeeds
+    stories={orderedStories}
+    initialStory={selectedStory}
+    currentUser={currentUser}
+    onClose={closeStoryFeeds}
+    onProfileClick={(id) => openProfile(id)}
+    onReply={replyToStory}
+    onLike={likeStory}
+    onReaction={reactToStory}
+    onFollow={followUser}
+    checkIsFollowing={checkIsFollowing}
+    followLoading={followLoading}
+    onFetchViewers={fetchStoryViewers}
+    muted={storyMuted}
+    onToggleMute={() => setStoryMuted(!storyMuted)}
+    onDeleteStory={deleteStory}
+    deleteLoading={deleteStoryLoading}
+  />
+)}
+
     {isChatsListOpen && currentUser && (
       <ChatsList
         currentUser={currentUser}
