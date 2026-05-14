739d32008ab7fc35a29fe239c0f64e86e0b16a06
Jastin123423 <Jjovinatha@gmail.com>
2026-05-14 10:41:44 +0300

    Update Story.tsx

--- components/Story.tsx
+++ components/Story.tsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect, useRef, useMemo, useCallback } from 'react'
 
 // -------------------- ADDED: Import ranking utility --------------------
 import { rankStoriesForReel } from '../utils/ranking';
-import { apiFetch, avatarFrom, formatRelativeTime, RichText } from './Feed';
+import { avatarFrom, formatRelativeTime, RichText } from './Feed';
 
 // -------------------- ADDED: Import filters --------------------
 import Filters, { 
